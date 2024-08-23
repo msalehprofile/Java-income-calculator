dbc01139bf92eee89fa5ed6f70c7f0b86e08690f
msalehprofile <mollyhannia@hotmail.co.uk>
2024-08-23 12:57:21 +0100

    added all tax calcs:

---
+++ src/main/java/org/example/IncomeCalculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class IncomeCalculator {
+    private static long userIncome;
+
+    public static void calculateTakeHomePay() {
+        User.getUserIncome();
+        MathUtilsTax.calculateIncomeTax();
+        User.shareIncomeAfterTax();
+
+    }
+
+
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class User {
+    private static String userInput;
+    private static long userGrossIncome;
+    private static double postTaxIncome;
+
+    public static String getUserInput() {
+        return userInput;
+    }
+
+    public static long getUserGrossIncome() {
+        return userGrossIncome;
+    }
+
+    public static void getUserIncome() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your annual income");
+        userGrossIncome = Long.parseLong(scanner.nextLine());
+
+    }
+
+    public static void shareIncomeAfterTax() {
+        postTaxIncome = MathUtilsTax.getUserIncomeAfterTax();
+        System.out.println("\nAfter tax your take home pay is:" +
+                "\nAnnually: " + postTaxIncome +
+                "\nMonthly: " + postTaxIncome/12
+        );
+    }
+}
